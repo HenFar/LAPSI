95c75059cef5c40b13d77351714754e448679a68
HenFar <50184607+HenFar@users.noreply.github.com>
2025-05-16 20:59:33 +0100

    Update test.m
    added the final tester for the final eemd function also in the folder, using a sine + noise signal. also added commentary to the code so as to make it more readable.

--- MLGeneral/Code/test.m
+++ MLGeneral/Code/test.m
@@ -1,9 +1,16 @@
-t = linspace(0, 1, 1000);
-signal = sin(2*pi*t)+0.3*randn(size(t));
+% tester for the eemd funnction also in the present folder.
+% uses a sin signal and adds to it a random noise, whose strength may be varied.
+% its supposed to print the original signal, the n (n = 6 here) IMFs selected and the residue of the transformation, 
+% along with adding both the imfs and residue to the workspace. imfs should be a large matrix and residue should 
+% present as a vector.
 
-[imfs, ort, nb_iter] = emd(signal);
-figure, plot(t, signal);
-legend('signal'); title('main signal');
+t = linspace(0, 1, 1000);   % x axis 
 
-figure, plot(t, imfs(1:3,:)');
-legend('IMF1', 'IMF2', 'IMF3'); title('First 3 IMFs');
+noise_strength = 0.2;   % define noise strength
+signal = sin(2*pi*t)+noise_strength*randn(size(t));    % sin signal with random noise
+
+L = 200;
+r = 0.2;
+n = 6;
+
+[imfs, residue] = eemd(signal, L, r, n);
